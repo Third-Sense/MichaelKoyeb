335b43130379a85a73cdb8cb625437ee6802c223
LᴀL <89661349+Devikaiii@users.noreply.github.com>
2022-12-03 19:18:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Devikaiii/Michael.git /Michael
+  git clone https://github.com/Devikaiii/Example.git /Example
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Michael
+  git clone $UPSTREAM_REPO /Example
 fi
-cd /Michael
+cd /Example
 pip3 install -U -r requirements.txt
 echo "Starting ᗩᒍᗩ᙭....🔥"
 python3 bot.py
