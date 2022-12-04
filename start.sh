97b36296d2d31b0d23f7036760ebac09f1ed369b
Third-Sense <108891477+Third-Sense@users.noreply.github.com>
2022-12-04 08:51:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-...
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Devikaiii/Michael.git /Michael
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Michael
+fi
+cd /Michael
+pip3 install -U -r requirements.txt
+echo "Starting ᗩᒍᗩ᙭....🔥"
+python3 bot.py
